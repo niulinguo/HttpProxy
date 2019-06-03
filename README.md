fc4a3e378794c3874e50a858d1ed5658ad49e7b1
niulinguo <niulinguo@163.com>
2019-06-03 22:08:54 +0800

    3.3.40

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 keytool -importcert -v -trustcacerts -alias baidu -file ./baidu.com.cer -keystore ./baidu.bks -storetype BKS -providerclass org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath ./bcprov-jdk15on-146.jar -storepass baidu
+
+
+
+
+
+
+openssl x509 -inform DER -in /Users/lingo.niu/Desktop/baidu.com.cer -out /Users/lingo.niu/Desktop/baidu.com.crt
--- app/src/main/java/com/lingo/httpproxy/MainActivity.java
+++ app/src/main/java/com/lingo/httpproxy/MainActivity.java
@@ -5,27 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.lidroid.xutils.HttpUtils;
-import com.lidroid.xutils.exception.HttpException;
-import com.lidroid.xutils.http.ResponseInfo;
-import com.lidroid.xutils.http.callback.RequestCallBack;
-import com.lidroid.xutils.http.client.HttpRequest;
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
-import org.apache.http.conn.ssl.SSLSocketFactory;
-
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Proxy;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
-    private HttpUtils mHttpUtils;
+
+    private X509Certificate getServerCert() throws IOException, CertificateException {
+
+        String certName = "baidu.com.crt";
+
+        InputStream inputStream = new BufferedInputStream(getAssets().open(certName));
+
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+
+        return (X509Certificate) certificateFactory.generateCertificate(inputStream);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +47,38 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = (TextView) findViewById(R.id.tv_text);
 
-        mHttpUtils = new HttpUtils();
-        mHttpUtils.configSSLSocketFactory(newSSLSocketFactory());
+        x.Ext.init(getApplication());
+        x.Ext.setDebug(BuildConfig.DEBUG);
+
     }
 
 
-    private SSLSocketFactory newSSLSocketFactory() {
+    private SSLSocketFactory newSSLSocketFactory() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, KeyManagementException {
 
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("cert", getServerCert());
+
+
+        String defaultAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(defaultAlgorithm);
+        trustManagerFactory.init(keyStore);
+
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+
+        return sslContext.getSocketFactory();
+    }
+
+    public void requestHttp(View view) {
+        mTextView.setText("");
+
+        RequestParams params = new RequestParams("https://www.baidu.com?v=1");
+        params.setProxy(Proxy.NO_PROXY);
         try {
-            KeyStore keyStore = KeyStore.getInstance("BKS");
-
-            InputStream inputStream = getResources().openRawResource(R.raw.baidu);
-
-            keyStore.load(inputStream, null);
-
-            inputStream.close();
-
-            SSLSocketFactory sslSocketFactory = new SSLSocketFactory(keyStore);
-
-            sslSocketFactory.setHostnameVerifier(SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
-
-            return sslSocketFactory;
-
+            params.setSslSocketFactory(newSSLSocketFactory());
         } catch (KeyStoreException e) {
             e.printStackTrace();
         } catch (CertificateException e) {
@@ -64,38 +87,30 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (UnrecoverableKeyException e) {
-            e.printStackTrace();
         } catch (KeyManagementException e) {
             e.printStackTrace();
         }
 
-        return null;
-    }
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                mTextView.setText(result);
+            }
 
-    public void requestHttp(View view) {
-        mTextView.setText("");
-        mHttpUtils.send(HttpRequest.HttpMethod.POST,
-                "https://www.baidu.com",
-                new RequestCallBack<String>() {
-                    @Override
-                    public void onLoading(long total, long current, boolean isUploading) {
-//                        mTextView.setText(current + "/" + total);
-                    }
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                mTextView.setText(ex.toString());
+            }
 
-                    @Override
-                    public void onSuccess(ResponseInfo<String> responseInfo) {
-                        mTextView.setText(responseInfo.result);
-                    }
+            @Override
+            public void onCancelled(CancelledException cex) {
 
-                    @Override
-                    public void onStart() {
-                    }
+            }
 
-                    @Override
-                    public void onFailure(HttpException error, String msg) {
-                        mTextView.setText(error.toString());
-                    }
-                });
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 }
